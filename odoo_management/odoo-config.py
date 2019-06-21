24e3cfe63158b1059740f0f588be5096820234e5
Antonio Pérez Ruth <anton@WKS02.makrin.es>
2019-06-21 21:04:04 +0200

    Initial
    Test

---
+++ odoo_management/odoo-config.py
@@ -0,0 +1,12 @@
+# File with Data Arrays
+
+# Versions
+# Organizations
+# Repos
+# Modules
+
+# versions to manage: v9, v10, v11
+# organizations to manage: oca, odoo, makrin
+# repos to manage for any organizations
+# modules to manage for any repo
+
